f8d72d87a0d746c8a6cf5cca2d660a1fa22679be
Aniruddh Tiwari <anni02.tiwari@gmail.com>
2025-09-23 21:33:46 +0530

    Resolve merge conflicts in App.js - include ChatScreen import and navigation

--- App.js
+++ App.js
@@ -5,11 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './Screens/LoginScreen';
 import RegistrationScreen from './Screens/RegistrationScreen';
 import GroupListScreen from './Screens/GroupListScreen'; 
-<<<<<<< HEAD
 import ChatScreen from './Screens/ChatScreen';
-=======
-//import ChatScreen from './Screens/ChatScreen';
->>>>>>> e4110ad7cf14606ed29b63bae40089b628a401eb
 
 const Stack = createStackNavigator();
 
@@ -23,11 +19,7 @@ const App = () => {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegistrationScreen} />
         <Stack.Screen name="GroupList" component={GroupListScreen} /> 
-<<<<<<< HEAD
         <Stack.Screen name="Chat" component={ChatScreen} />
-=======
-        {/* <Stack.Screen name="Chat" component={ChatScreen} /> */}
->>>>>>> e4110ad7cf14606ed29b63bae40089b628a401eb
       </Stack.Navigator>
     </NavigationContainer>
   );
